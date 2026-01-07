e85bd8cee1c1f11b15bea9c857acc637f916aa57
AzPepoze <pladumguy@gmail.com>
2026-01-08 00:42:28 +0700

    clean up tray.py

--- src/linux/tray.py
+++ src/linux/tray.py
@@ -2,34 +2,30 @@ import sys
 import os
 import signal
 import gi
+from config import APP_NAME
 
-gi.require_version('Gtk', '3.0')
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
-
-# Default App Name
-APP_NAME = "BrowserAsWallpaper"
-
-# Get info from arguments if available
-# argv[1] = parent_pid, argv[2] = app_name
-if len(sys.argv) > 2:
-    APP_NAME = sys.argv[2]
 
 GLib.set_prgname(APP_NAME)
 GLib.set_application_name(APP_NAME)
 
 APP_INDICATOR = None
 try:
-    gi.require_version('AyatanaAppIndicator3', '0.1')
+    gi.require_version("AyatanaAppIndicator3", "0.1")
     from gi.repository import AyatanaAppIndicator3 as AppIndicator
+
     APP_INDICATOR = AppIndicator
 except ValueError:
     try:
-        gi.require_version('AppIndicator3', '0.1')
+        gi.require_version("AppIndicator3", "0.1")
         from gi.repository import AppIndicator3 as AppIndicator
+
         APP_INDICATOR = AppIndicator
     except ValueError:
         print("[Tray] Error: AppIndicator library not found.")
         sys.exit(1)
+
 
 def quit_app(source):
     if len(sys.argv) > 1:
@@ -40,30 +36,29 @@ def quit_app(source):
             pass
     Gtk.main_quit()
 
+
 def build_menu():
     menu = Gtk.Menu()
     item_quit = Gtk.MenuItem(label=f"Quit {APP_NAME}")
-    item_quit.connect('activate', quit_app)
+    item_quit.connect("activate", quit_app)
     menu.append(item_quit)
     menu.show_all()
     return menu
+
 
 def main():
     if APP_INDICATOR is None:
         return
 
     indicator_id = f"{APP_NAME.lower().replace(' ', '-')}-tray"
-    indicator = APP_INDICATOR.Indicator.new(
-        indicator_id,
-        "preferences-desktop-wallpaper",
-        APP_INDICATOR.IndicatorCategory.APPLICATION_STATUS
-    )
+    indicator = APP_INDICATOR.Indicator.new(indicator_id, "preferences-desktop-wallpaper", APP_INDICATOR.IndicatorCategory.APPLICATION_STATUS)
     indicator.set_status(APP_INDICATOR.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     indicator.set_label(APP_NAME, APP_NAME)
-    
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     Gtk.main()
+
 
 if __name__ == "__main__":
     main()
